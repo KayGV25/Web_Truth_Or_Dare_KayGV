b414a4f9080597e72d65fc0dbfd1c6b65b278b69
KayGV25 <85599037+KayGV25@users.noreply.github.com>
2024-02-01 16:01:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+import {Truth, Dare} from "./Lists.js"
+let deck = []
+let min_length = Math.min(Truth.length,Dare.length);
+let current = -1;
+for(var i=0; i<min_length; i++){
+    deck.push(i);
+}
+
+function Display(current){
+    var q_Truth = Truth[current];
+    var q_Dare = Dare[current];
+    var truth = document.getElementById("truth");
+    var dare = document.getElementById("dare");
+    truth.innerHTML = q_Truth;
+    dare.innerHTML = q_Dare;
+
+}
+function Next(){
+    current++;
+    Display(current);
+}
+function Shuffle(){
+    console.log(deck);
+    for(var i=min_length-1; i>=0; i--){
+        var randomIndex = Math.floor(Math.random()*1000 % min_length);
+        [deck[i], deck[randomIndex]] = [deck[randomIndex], deck[i]];
+    }
+    console.log(deck)
+}
+
+document.getElementById("Shuffle").addEventListener("click", Shuffle);
+document.getElementById("Next").addEventListener("click", Next);
